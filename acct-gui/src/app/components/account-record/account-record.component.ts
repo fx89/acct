b8339a95eb665f17a521a0f79265327634b1f69c
Catalin Ioan Georgescu <catalin.ioan.georgescu@gmail.com>
2022-08-09 19:52:43 +0300

    Adding popups to foreign currency accounts to inform the user of gains / losses due to exchange rates

--- acct-gui/src/app/components/account-record/account-record.component.ts
+++ acct-gui/src/app/components/account-record/account-record.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { IncomeOrExpenseItem } from 'src/app/model/income-or-expense-item';
 import { StateService } from 'src/app/services/state-service/state.service';
 import { AccountRecord } from 'src/app/model/account-record';
+import * as uuid from 'uuid';
 
 @Component({
   selector: 'app-account-record',
@@ -11,6 +12,10 @@ import { AccountRecord } from 'src/app/model/account-record';
 export class AccountRecordComponent implements OnInit {
 
     @Input() rec : AccountRecord;
+
+    id : string = "_" + uuid.v4();
+
+    private previouslyShownExchangeRateDetailsBoxId
 
     constructor(private state : StateService) {
 
@@ -50,6 +55,16 @@ export class AccountRecordComponent implements OnInit {
         return rec.value > 0;
     }
 
+    getLastRecordedExchangeRate() {
+        const currency = this.state.getMonitoiredCurrency(this.state.getAccount(this.rec.accountId).currencyId)
+
+        if (currency) {
+            return currency.lastCollectedValue
+        }
+
+        return 0
+    }
+
     public isSafeToExchangeBack(rec:AccountRecord) : boolean {
         const currency = this.state.getMonitoiredCurrency(this.state.getAccount(rec.accountId).currencyId)
 
@@ -59,5 +74,50 @@ export class AccountRecordComponent implements OnInit {
         }
 
         return false;
+    }
+
+    public getExchangeRateTextboxId() {
+        return this.id + "_exchangeRateTextbox"
+    }
+
+    public getExchangeRateDetailsBoxId() {
+        return this.id + "_exchangeRateDetailsBox"
+    }
+
+    public showExchangeRateDetailBox(event, exchangeRateDetailsBoxId) {
+        this.hideAllExchangeRateDetailsBoxes()
+
+        if (this.isIncome(this.rec)) {
+            const detailsBox = document.getElementById(exchangeRateDetailsBoxId)
+
+            detailsBox.style.left = (event.x - 190) + "px"
+            detailsBox.style.top = (event.y - 70) + "px"
+
+            detailsBox.style.visibility = "visible"
+
+            this.previouslyShownExchangeRateDetailsBoxId = exchangeRateDetailsBoxId
+        }
+    }
+
+    public hideExchangeRateDetailsBox(exchangeRateDetailsBoxId) {
+        const detailsBox = document.getElementById(exchangeRateDetailsBoxId)
+        detailsBox.style.visibility = "hidden"
+    }
+
+    public hideAllExchangeRateDetailsBoxes() {
+        const elements = document.getElementsByClassName("account-record-exchange-rate-popup")
+
+        for (let i = 0 ; i < elements.length ; i++) {
+            const element : any = elements.item(i)
+            element.style.visibility = "hidden"
+        }
+    }
+
+    public getRecValue() {
+        return this.rec ? this.rec.value : 0
+    }
+
+    public getRecExchangeRate() {
+        return this.rec ? this.rec.exchangeRate : 0
     }
 }
