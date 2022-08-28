a9525289a573605a1e2a6b2e36530bf3b6dac5b1
Catalin Ioan Georgescu <catalin.ioan.georgescu@gmail.com>
2022-08-28 21:06:08 +0300

    Disabling CSRF

--- acct/acct-ws/src/main/java/com/desolatetimelines/acct/config/WebSecurityConfig.java
+++ acct/acct-ws/src/main/java/com/desolatetimelines/acct/config/WebSecurityConfig.java
@@ -1,11 +1,18 @@
 package com.desolatetimelines.acct.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -18,12 +25,35 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         this.appuserProperties = appuserProperties;
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
             .inMemoryAuthentication()
             .passwordEncoder(passEnc)
             .withUser(appuserProperties.getUsername()).password(appuserProperties.getPassword()).roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     private static class NonEncodingPasswordEncoder implements PasswordEncoder {
