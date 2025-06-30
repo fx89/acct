aa63cf06101631308722e9f7944b620608758919
Catalin Ioan Georgescu <>
2025-06-30 20:54:02 +0300

    BUG-00006: Bar (top, bottom, floating, auto-hiding): the spacer between the bar and the rest of the content is visible

--- docs/bugs-todo.java
+++ docs/bugs-todo.java
@@ -3,3 +3,4 @@
 // DONE: BUG-00003: Dropdown with cards list: options list contained into parent element instead of floating above
 // DONE: BUG-00004: REST APIs require authentication for preflight requests
 // DONE: BUG-00005: WorkspaceCollectionsResponse: inconsistent field names
+// DONE: BUG-00006: Bar (top, bottom, floating, auto-hiding): the spacer between the bar and the rest of the content is visible
